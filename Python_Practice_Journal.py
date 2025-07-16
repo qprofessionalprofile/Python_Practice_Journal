a7a9e57def482aedc44da285274130d1092a2b18
qprofessionalprofile <rivers_professionalreach@pm.me>
2025-07-15 21:34:53 -0500

    First commit of my practice journal for Python

---
+++ Python_Practice_Journal.py
@@ -0,0 +1,87 @@
+# userScoreCheck = int(input("Hi what score did you recieve?"))
+
+# if userScoreCheck == 100:
+#     print("You passed! Hunids all around!")
+# elif userScoreCheck == 70:
+#     print("You passed... But barely..hmmmm")
+# else: 
+#     print("You failed")
+    
+# message = "Hello World"
+# print(message.upper())#uppercases letters
+# print(message.lower())#lowercases letters
+
+
+
+#-- Practice with a few methods
+
+# text = "  Python is Cool!!    "
+# print(text.strip()) # Prints the string without whitespaces Beginning or end
+
+# message = "Hello World!" 
+# print(message.replace("World", "goose"))# Prints the string with replaces string parts.
+
+# sentence = "Python Practice is always fun with lists"
+# words = sentence.split() # Will print a list with all words of the string stored inside
+
+# words = ['Python', 'Practice', 'To', 'get', 'Noticed']
+# sentence = "".join(words) # Joins the words of the list into a string
+
+# filename = "report.pdf"
+# print(filename.endswith(".pdf")) # Checks what the string ends with and returns a boolean.
+
+
+
+#--Further practice using strip, replace, split, and upper.
+
+# my_String = '   Might I be an engineer?????   '
+
+# string_Edit_One = print(my_String.strip())
+# string_Edit_Two = print(my_String.replace("Might I be an engineer?????", "Coding Temple Trained!!"))
+# string_Edit_Three = print(my_String.split())
+# string_Edit_Four = print(my_String.upper())
+
+
+
+#-- #Use of dynamic insertion for formating variable into strings
+
+# name = "Q"
+# age = 100000
+# occupation = "Student Engineer"
+# print("My name is {} and I am  {} years old!! I am a {}.".format(name, age, occupation)) # My silly use of the format method
+
+
+#-- Working with a random name generator
+
+# import random
+
+# first_names_list = ['Jennifer', 'Lisa', 'John', 'Rakesh']
+# last_name_list = ['Patel', 'Micheals', 'Gonzales', 'Smith']
+
+# first_names_list = random.choice(first_names_list)
+# last_name_list = random.choice(last_name_list)
+
+# name_mixer = print(f"{first_names_list} {last_name_list}")
+
+
+
+# #String conversion to Integer
+# num_str = "123"
+# num = print(int(num_str))
+
+
+first_num = int(input("What is your first number?"))
+second_num = int(input("What is your second number"))
+math_choice = input("waht kinda math you doin today?")
+
+math_choice = if math_choicea
+
+calc_list = [
+addition = first_num + second_num,
+subtract = first_num - second_num,
+multiply = first_num * second_num,
+divide = first_num / second_num]
+    
+def calc_fun(calc_List) :
+    if calc_List == calc_List[0]:
+        print
