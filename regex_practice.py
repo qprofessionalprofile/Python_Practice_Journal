3c4ce523795140c1108d2c180f2a7d640b8c47fe
qprofessionalprofile <rivers_professionalreach@pm.me>
2025-08-13 23:58:04 -0500

    8/13/25 I've added new practice with re.sub() and re.search along with their respective comments. On 8/14/25 I'll go ahead and add new information on things like file handling with python.

--- regex_practice.py
+++ regex_practice.py
@@ -62,36 +62,68 @@ Practice using Pythons regular expressions. I'm practicing functions like findal
 #     ("Protocol not found")
 
     
-"""
-Practice with re.split()
-Splits strings into a list of substrings based on occurences
-of a specified pattern in  a program. 
-Similar in nature to the built in str.split() but with re.split() regex rules 
-makes for more complex splitting. Eg. splitting mulitple delimeters or 
-patterns.
-"""
+# """
+# Practice with re.split()
+# Splits strings into a list of substrings based on occurences
+# of a specified pattern in  a program. 
+# Similar in nature to the built in str.split() but with re.split() regex rules 
+# makes for more complex splitting. Eg. splitting mulitple delimeters or 
+# patterns.
+# """
 
-info = 'Regex, in, python,s plitting, example'
+# info = 'Regex, in, python,s plitting, example'
 
-splitter = re.split(r"[,.;\s!-]+", info)
-print(splitter)
-
-
-
-"""
-Practice with comma seperated values. CSV for short.
-
-Splitting based on the commas passed to the r, from CSV_info
-"""
-
-CSV_info = "one,two,three"
-CSV_Split = re.split(r",", CSV_info) 
-print(CSV_Split)
+# splitter = re.split(r"[,.;\s!-]+", info)
+# print(splitter)
 
 
 
-"""
-Manipulating and subbing text with the
-re.sub(pattern, text, replacer) function.
-"""
+# """
+# Practice with comma seperated values. CSV for short.
 
+# Splitting based on the commas passed to the r, from CSV_info
+# """
+
+# CSV_info = "one,two,three"
+# CSV_Split = re.split(r",", CSV_info) 
+# print(CSV_Split)
+
+
+
+# """
+# Manipulating and subbing text with the
+# re.sub(pattern, text, replacer) function.
+# """
+
+# phone_number = "(555)555-5555"
+# formatted_number = re.sub(r"\D", '', phone_number) 
+# #returns an empty string for everything thats not a digit.
+# print(formatted_number)
+
+# chat = '''
+# @user_person1 : "This is a placeholder message!"
+# @user-person2 : "Wow! another string!!"
+# @user_persong-3 : "Hehehe I think im getting the hang of this!"
+# '''
+# anonomous_chat_creation = re.sub(r"@[\w-]+", '@anonomous_user', chat)
+# print(anonomous_chat_creation)
+
+
+
+
+# """
+# Matching data using the () with regular expressions.
+#  re.search() is used to refer to a group by index using (). 
+#  It looks like re/search(pattern, text).group(index)
+#  can be used in finding parts of a phone number. 
+#  You can also use it to get and manipulate parts of a string.
+#  URL's, Phone Numbers, dates and allows you to use them.
+# """
+
+# phone_number_text = "124-816"
+# find_pattern = r"(\d+)-(\d+)"
+# found_match = re.search(find_pattern, phone_number_text)
+# if found_match:
+#     print(f"Int_group 1: {found_match.group(1)}")
+#     print(f"Int_group 2: {found_match.group(2)}")
+    
