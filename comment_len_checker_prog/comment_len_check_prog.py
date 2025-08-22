6d6328bbebcef2af6002087500a557de15c97244
qprofessionalprofile <rivers_professionalreach@pm.me>
2025-08-22 17:12:51 -0500

    8/22/25 Revamped comment checker set up! With a README addition. Check README for anticipated changes and use cases.

--- comment_len_checker_prog/comment_len_check_prog.py
+++ comment_len_checker_prog/comment_len_check_prog.py
@@ -1,5 +1,10 @@
-"""#this is a micro program to print the length of my comments 
-to keep me in the 78-80 range per line"""
+"""# This is a micro program to print the length of comments to keep them pep-8 range per line.
+--white spaces count!--"""
 
-com_len = ""
-print(len(com_len))
+comment = ""# <--paste your code here
+com_len = len(comment)
+
+if com_len <= 78 | 80:
+    print(f"Your comment length is: {com_len}, you're in the safe range")
+else:
+    print("Your comments too long! Try a multi line comment or reduce your words to 78 - 80 characters.")
